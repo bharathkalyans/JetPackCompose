713652eb4a0a4dd277f9ba62a6ff30bc824715e3
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-22 10:27:36 +0530

    Working on Rows and Columns!

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.demoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.demoapp.ui.theme.DemoAppTheme
 
@@ -14,25 +22,28 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DemoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background ,)  {
-                    Greeting("Bharath Kalyan S")
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Companion.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("Hello ")
+                Text("World")
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Yellow),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Hello ")
+                Text("World")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DemoAppTheme {
-        Greeting("Bharath")
-    }
-}
