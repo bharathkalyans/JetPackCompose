21a9c67fbc373c2c98ffbd107f6790b3656e6c9a
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-22 13:00:17 +0530

    Created a Image Card! using Composable!

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -1,50 +1,103 @@
 package com.example.demoapp
 
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.example.demoapp.ui.theme.DemoAppTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Yellow)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .border(width = 3.dp, color = Color.Red, shape = RectangleShape)
-                    .padding(20.dp, 40.dp)
-            ) {
-                Text("Hello ",modifier = Modifier.clickable {
-                    Toast.makeText(this@MainActivity, "Clicked Hello", Toast.LENGTH_SHORT).show()
-                })
-                Spacer(modifier = Modifier.height(20.dp))
-                Text("World")
+            val painter = painterResource(id = R.drawable.two)
+            val title = "Kermit drinking Green Tea!"
+            val description = "Kermit is Enjoying XD"
 
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.7f)
+                    .fillMaxHeight(0.3f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(
+                    painter = painter,
+                    title = title,
+                    contentDescription = description,
+                )
             }
 
-        }
 
+        }
     }
+
+    @Composable
+    fun ImageCard(
+        painter: Painter,
+        title: String,
+        contentDescription: String,
+        modifier: Modifier = Modifier
+    ) {
+        Card(
+            modifier = modifier,
+            shape = RoundedCornerShape(20.dp),
+            elevation = 5.dp
+        ) {
+            Box(modifier = Modifier.height(400.dp)) {
+                //Image
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+                )
+                //Using for Gradient!
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(Color.Transparent, Color.Black),
+                                startY = 300f
+                            )
+                        )
+                )
+                //Text
+                Text(
+                    text = title,
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(16.dp),
+                    color = Color.White
+                )
+                /*Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    Text(
+                        text = title,
+                        style = TextStyle(color = Color.White, fontSize = 16.sp)
+                    )
+                }*/
+            }
+        }
+    }
+
 }
 
