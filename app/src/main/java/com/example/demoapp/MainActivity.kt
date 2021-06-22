1e586caeca66fa2582c7e3a78f069f6eadd5cc5d
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-22 11:57:00 +0530

    Done with Modifiers!

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.demoapp
 
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,7 +16,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.demoapp.ui.theme.DemoAppTheme
@@ -25,17 +30,16 @@ class MainActivity : ComponentActivity() {
             Column(
                 modifier = Modifier
                     .background(Color.Yellow)
-                    .padding(20.dp, 40.dp)
                     .fillMaxHeight(0.5f)
                     .fillMaxWidth()
+                    .border(width = 3.dp, color = Color.Red, shape = RectangleShape)
+                    .padding(20.dp, 40.dp)
             ) {
-                Text(
-                    "Hello ", modifier = Modifier
-                        .offset(0.dp,20.dp),
-                    color = Color.Red,
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-                Text("World ")
+                Text("Hello ",modifier = Modifier.clickable {
+                    Toast.makeText(this@MainActivity, "Clicked Hello", Toast.LENGTH_SHORT).show()
+                })
+                Spacer(modifier = Modifier.height(20.dp))
+                Text("World")
 
             }
 
