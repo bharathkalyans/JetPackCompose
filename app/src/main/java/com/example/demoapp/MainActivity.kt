01023811eb8ffc81151db9f5640163667bac73d4
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-29 11:22:49 +0530

    List in Compose (Similar to recycler Views and fare better than it actually)

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.demoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 
 
@@ -17,50 +22,72 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
-            val scope = rememberCoroutineScope()
 
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(30.dp)
-                ) {
-                    TextField(
-                        value = textFieldState,
-                        label = {
-                            Text(text = "Enter Name")
-                        },
-                        onValueChange = {
-                            textFieldState = it
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
+            LazyColumn {
+                itemsIndexed(
+                    listOf(
+                        "Bharath",
+                        "Kalyan",
+                        "is",
+                        "my",
+                        "Name!!",
+                        "Bharath",
+                        "Kalyan",
+                        "is",
+                        "my",
+                        "Name!!",
+                        "Bharath",
+                        "Kalyan",
+                        "is",
+                        "my",
+                        "Name!!",
+                        "Bharath",
+                        "Kalyan",
+                        "is",
+                        "my",
+                        "Name!!"
                     )
-                    Spacer(modifier = Modifier.height(15.dp))
-                    Button(
-                        onClick = {
-                            scope.launch {
-                                scaffoldState
-                                    .snackbarHostState
-                                    .showSnackbar("Hello $textFieldState")
-                            }
-                        },
-
-                    ) {
-                        Text(text = "Greetings")
-                    }
+                ) { index, string ->
+                    Text(
+                        text = string,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(24.dp)
+                    )
+                }
+                items(5000) {
+                    Text(
+                        text = "Item $it",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(24.dp)
+                    )
                 }
             }
 
+            //Not good as Performance wise as all Items are loaded on start time!
+            /*val scrollState = rememberScrollState()
+            Column(
+                modifier = Modifier.verticalScroll(scrollState)
+            ) {
+                for (i in 1..51) {
+                    Text(
+                        text = "Item $i",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(24.dp)
+                    )
+                }
+            }*/
         }
 
     }
